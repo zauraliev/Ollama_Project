c50584c95c147ec0385c911f25331290b941fdfe
Zaur <zaur_aliev@hotmail.com>
2025-12-07 04:12:25 -0500

    Ollama model wiseolio

--- app-2.py
+++ app-2.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import ollama
 
 # res = ollama.chat(
@@ -5,11 +6,25 @@ import ollama
 #   messages=[{"role": "user", "content": "Tell me a fun fact about Baku"}]
 # )
 
-res = ollama.chat(
-  model="llama3.2",
-  messages=[{"role": "user", "content": "Why is the ocean so salty?"}],
-  stream=True
-)
+# res = ollama.chat(
+#   model="llama3.2",
+#   messages=[{"role": "user", "content": "Why is the ocean so salty?"}],
+#   stream=True
+# )
 
-for chunk in res:
-  print(chunk["message"]["content"], end="", flush=True)
+# for chunk in res:
+#   print(chunk["message"]["content"], end="", flush=True)
+
+ollama.create(
+    model='wiseolio',
+    from_='llama3.2',
+    system="You are Wise Olio, very smart assistant which answers questions intuitively",
+    parameters={
+        'temperature': 0.1
+    }
+)
+res = ollama.generate(
+  model="wiseolio",
+  prompt="Why is the ocean so salty?",
+)
+print(res['response'])
